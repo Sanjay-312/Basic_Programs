f219e678398ae88153073991a897455c1dd50a12
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-09-05 08:27:42 +0530

    [sanjeeva] : checked viwel or not

---
+++ vowelOrNot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class vowelOrNot
+    {
+        public static void checkVowel()
+        {
+            Console.WriteLine("enter the character");
+            char character=Convert.ToChar(Console.ReadLine().ToLower());
+
+            switch (character)
+            {
+                case 'a':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("vowel");
+                    break;
+                default:
+                    Console.WriteLine("consonant");
+                    break;
+
+            }
+        }
+    }
+}
